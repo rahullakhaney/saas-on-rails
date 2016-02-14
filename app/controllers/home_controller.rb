6defd72d317c5d9d9c70392e0b698e898587d4aa
rahullakhaney <rahul@codebrew.io>
2016-02-14 13:33:27 +0530

    Updated home and registrations controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,8 +3,6 @@ class HomeController < ApplicationController
 		@message = "Welcome User"
 		if current_user
 			redirect_to dashboard_path
-		else
-			redirect_to timeline_path
 		end
 	end
 
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -17,15 +17,11 @@ class RegistrationsController < Devise::RegistrationsController
     new_user_session_url(subdomain: resource.subdomain)
     flash[:info] = "Please check your mail for a confirmation link"
 
-  end
+end
 
-    def after_inactive_sign_up_path_for(resource)
-        new_user_session_url(subdomain: resource.subdomain)
-   end
+def after_inactive_sign_up_path_for(resource)
+	new_user_session_url(subdomain: resource.subdomain)
+end
 
-   def update_resource(resource, params)
-    resource.update_without_password(params)
-  end
 
-   
 end
